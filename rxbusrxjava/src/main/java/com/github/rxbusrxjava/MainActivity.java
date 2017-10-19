f1616a6f6dd5b8b28e2c4108c58f2e6aa1d9c790
zhanggang <zhang834826337@qq.com>
2017-10-19 15:46:11 +0800

    各个demo-rxjava和rxbus

---
+++ rxbusrxjava/src/main/java/com/github/rxbusrxjava/MainActivity.java
@@ -0,0 +1,115 @@
+package com.github.rxbusrxjava;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Predicate;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "TAG";
+    private ObservableEmitter<Integer> emitter;
+    int s = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //几秒后接收事件
+        sample();
+        //接收指定的事件
+//        filter();
+        //去重复
+        distinct();
+    }
+
+    private void distinct() {
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(@NonNull ObservableEmitter<Integer> e) throws Exception {
+                e.onNext(5);
+                e.onNext(5);
+                e.onNext(6);
+            }
+        }).distinct()  //去重复
+                .subscribe(new Consumer<Integer>() {
+                    @Override
+                    public void accept(@NonNull Integer integer) throws Exception {
+                        Log.e(TAG, "accept: " + integer);
+                    }
+                });
+
+    }
+
+    private void filter() {
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(@NonNull ObservableEmitter<Integer> e) throws Exception {
+                for (int i = 0; i < 20; i++) {
+                    e.onNext(i);
+                }
+            }
+        }).filter(new Predicate<Integer>() {
+            @Override
+            public boolean test(@NonNull Integer integer) throws Exception {
+                return integer % 2 != 0;  //奇数
+            }
+        }).subscribe(new Consumer<Integer>() {
+            @Override
+            public void accept(@NonNull Integer integer) throws Exception {
+                Log.e(TAG, "accept: " + integer);
+            }
+        });
+    }
+
+    //几秒后接收时间
+    private void sample() {
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(@NonNull ObservableEmitter<Integer> e) throws Exception {
+                emitter = e;
+            }
+        }).sample(5, TimeUnit.SECONDS)  //5秒后接收
+                .subscribe(new Observer<Integer>() {
+                    @Override
+                    public void onSubscribe(@NonNull Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(@NonNull Integer i) {
+                        Log.e(TAG, "onNext: " + i);
+                    }
+
+                    @Override
+                    public void onError(@NonNull Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+
+
+    }
+
+    //点击请求数据
+    public void onClick(View view) {
+        int m = s++;
+        Log.e(TAG, "emitter: " + m);
+        emitter.onNext(m);
+    }
+}
